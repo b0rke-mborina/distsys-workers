5b0a00321b739830fc6713210d20873fc65f3d50
b0rke-mborina <mborina@net.hr>
2023-02-04 12:11:38 +0100

    client script, one worker, master service, started connecting

--- client.py
+++ client.py
@@ -3,22 +3,61 @@ import aiohttp
 import pandas as pd
 
 
-print("client OK")
+print("Running client script...\n")
 
 # generate list of client IDs
 listOfClientIDs = list(range(1, 10001))
-print(listOfClientIDs)
+# print(listOfClientIDs)
 
 # load dataframe
 dataframe = pd.read_json("data/dataset.json", lines = True)
+# print(len(dataframe))
+rowsPerClient = int(len(dataframe) / len(listOfClientIDs))
+# print(rowsPerClient)
+
+# for index, row in dataframe.iloc[10:20].iterrows():
+	# print(row.get("content")[0:30])
+	# print("_______________________")
 
 # create dict for client IDs and their code
+clients = {id:[] for id in listOfClientIDs}
+for id, codes in clients.items():
+	# print("ID:", id, codes)
+	fromRow = (id - 1) * rowsPerClient
+	# print(fromRow)
+	toRow = fromRow + rowsPerClient
+	# print(toRow)
+	for index, row in dataframe.iloc[fromRow:toRow].iterrows():
+		codes.append(row.get("content"))
+	# print("Codes len:", len(codes))
 
-# send requests for code processing
+# print(clients[69])
+# print(len(clients[69]))
+from itertools import islice
+def take(n, iterable):
+	"""Return the first n items of the iterable."""
+	return dict(islice(iterable, n))
+clients = take(10, clients.items())
+
+# sending requests for code processing (variables, function, function call)
+tasks = []
+results = []
+
 async def processCode():
-	async with aiohttp.ClientSession() as session:
-		print("OK")
+	# make variables visible
+	global tasks
+	global results
 
-asyncio.run(processCode())
+	# send requests for code processing
+	async with aiohttp.ClientSession() as session:
+		for id, codes in clients.items():
+			tasks.append(asyncio.create_task(session.get("http://127.0.0.1:8080/", json = { "user": id, "codes": codes })))
+			print("Data for user", id, "with length", len(codes), "sent.")
+		results = await asyncio.gather(*tasks)
+		results = [await x.text() for x in results]
+		print("Results of data processing for all clients retrieved.\n")
+
+asyncio.get_event_loop().run_until_complete(processCode())
 
 # log average number of letters for clients' code
+print(results)
--- serviceMaster.py
+++ serviceMaster.py
@@ -1,22 +1,73 @@
 # import subprocess
 from aiohttp import web
+import random
+import time
+import asyncio
+import aiohttp
 
+
+masterPort = 8080
+
+maxNumberOfRequests = 100000
+requests = []
+
+# calculate number of workers N (random 5 - 10)
+N = random.randint(5, 10)
+print("Number of workers:", N)
+
+# call workers and give them IDs (save to dict)
+workers = {"workerWithId" + str(id): [] for id in range(1, N + 1)}
+print("Workers:", workers)
 
 routes = web.RouteTableDef()
 
-@routes.post("/")
+@routes.get("/")
 async def function(request):
 	try:
 		print("master OK")
-		# calculate number of workers N (random 5 - 10)
-		# call workers and give them IDs (save to dict)
+
+		# make variables visible
+		global maxNumberOfRequests
+		global requests
+		global N
+		global workers
+
+		# print data and status
+		data = await request.json()
+		print("User id:", data.get("user"))
+		print("Codes length:", len(data.get("codes")))
+		# for i in data:
+			# print(i[0:30])
+			# print("_______________________")
+		requests.append(data)
+		print("New data recieved. Current data status", len(requests), "/", maxNumberOfRequests)
+
 		# send 1000 lines to each worker
-		#  - log timestamps of tasks
-		#  - count received and done tasks
-		#  - send other lines to workers (100o to each) when tasks are completed
+		startTime = time.monotonic_ns()
+
+		# WORK IN PROGRESS
+		if len(requests) >= 10000:
+			tasks = []
+			async with aiohttp.ClientSession() as session:
+				for i in range(1, N + 1):
+					tasks.append(asyncio.create_task(session.get(f"http://127.0.0.1:{8080 + i}/")))
+				res = await asyncio.gather(*tasks)
+				res = [await x.text() for x in res]
+				print(res)
+			# for i in range(1, N + 1):
+				# task = asyncio.create_task(session.get("http://service0:8080/"))
+				# response = await asyncio.gather(task)
+				# responseData = await response[0].json()
+				# tasks = asyncio.create_task(*workers.get(f"workerWithId{i}"))
+			results = res
+			endTime = time.monotonic_ns()
+			print("Time:", endTime - startTime)
+			#  - log timestamps of tasks
+			#  - count received and done tasks
+			#  - send other lines to workers (1000 to each) when tasks are completed
 
 		# subprocess.call('python serviceWorker.py', creationflags=subprocess.CREATE_NEW_CONSOLE)
-		return web.json_response({"name": "master", "status": "OK"}, status = 200)
+		return web.json_response({"name": "master", "status": "OK", "averageWordcount": len(requests)}, status = 200) # sum(results) / len(results)
 	except Exception as e:
 		return web.json_response({"name": "master", "error": str(e)}, status = 500)
 
@@ -24,4 +75,4 @@ app = web.Application()
 
 app.router.add_routes(routes)
 
-web.run_app(app, port = 8080)
+web.run_app(app, port = masterPort)
